105c7829c24b352dc1544a1a36072500fbab14f6
abigailcuevas <77945958+abigailcuevas@users.noreply.github.com>
2021-04-12 18:58:19 -0700

    Add files via upload

---
+++ Week 10 - Texturing, Transparency/shaders/unlit.textured.fs.glsl
@@ -0,0 +1,25 @@
+precision mediump float;
+
+uniform sampler2D uTexture;
+//uniform sampler2D tempTexture;
+uniform float uAlpha;
+uniform float uTempAlpha;
+
+// todo #3 - receive texture coordinates and verify correctness by DONE
+// using them to set the pixel color 
+varying vec2 fragmentShader;
+
+void main(void) {
+    // todo #3 DONE
+   //gl_FragColor = vec4(fragmentShader,0.0,1.0);
+
+    // todo #5 DONE
+    //gl_FragColor=texture2D(uTexture,fragmentShader);
+
+    // todo #11 DONE
+    vec4 color1 = texture2D(uTexture, fragmentShader+(cos(uTempAlpha)/(3.14)/1.5));
+    //vec4 color2 = texture2D(uTexture, fragmentShader+(cos(uTempAlpha)/(3.14)/1.5));
+    gl_FragColor = color1;//*color2; 
+    gl_FragColor.a = uAlpha;
+
+}
